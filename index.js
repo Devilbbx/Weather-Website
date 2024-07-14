4ddff754a9adec58c8d3464282a23e8493c0a00a
Devendra Chapagain <97355982+Devilbbx@users.noreply.github.com>
2024-07-14 10:19:19 +0000

    chnages

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=Seattle';
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '698dfdc2efmsh720de8f5788e17ap1c992ejsn165779378097',
-		'x-rapidapi-host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
 
