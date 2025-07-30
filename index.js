d2945fc977034b7a3348b0df5904e3ab3284bb31
Devilbbx <devendrachapagain100@gmail.com>
2025-07-30 22:32:57 +0545

    shifted the project to next project

--- index.js
+++
@@ -1,7 +0,0 @@
-const options = {method: 'GET',
-    headers: {accept: 'application/json'}};
-    
-    fetch('https://api.tomorrow.io/v4/weather/forecast?location=newyork&apikey=XXX', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
